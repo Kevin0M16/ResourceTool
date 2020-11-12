cf9ab194c3efaaa381c8c734554eda446cdeee08
Kevin0M16 <kevin@nmscoordinates.com>
2020-11-12 15:16:01 -0600

    Update #2 v1.0.1
    Added more functionality

--- ResourceTool/Form1.Designer.cs
+++ ResourceTool/Form1.Designer.cs
@@ -36,36 +36,41 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(56, 404);
+            this.button1.Location = new System.Drawing.Point(56, 406);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(110, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Find";
+            this.button1.Text = "Find Vehicles";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(4, 40);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(256, 20);
+            this.textBox2.Size = new System.Drawing.Size(469, 20);
             this.textBox2.TabIndex = 2;
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(218, 405);
+            this.textBox3.Location = new System.Drawing.Point(430, 407);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(44, 20);
             this.textBox3.TabIndex = 3;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(6, 405);
+            this.textBox4.Location = new System.Drawing.Point(6, 407);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(44, 20);
             this.textBox4.TabIndex = 4;
@@ -76,18 +81,18 @@
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(256, 333);
+            this.textBox1.Size = new System.Drawing.Size(469, 333);
             this.textBox1.TabIndex = 5;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(137, 404);
+            this.button2.Location = new System.Drawing.Point(349, 406);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 6;
             this.button2.Text = "Copy Text";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -100,11 +105,66 @@
             this.label1.Text = "vMenu addons.json Tool";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(172, 456);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Find Filenames";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(172, 406);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Find Resources";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(172, 431);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Find Foldernames";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(56, 431);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Find ymaps";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(56, 456);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(110, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Make popgroup";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(267, 430);
+            this.ClientSize = new System.Drawing.Size(476, 481);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
@@ -129,6 +189,11 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- ResourceTool/Form1.cs
+++ ResourceTool/Form1.cs
@@ -27,6 +27,12 @@ namespace ResourceTool
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Clipboard.SetText(textBox1.Text);
+            MessageBox.Show("Copied to Clipboard!", "Clipboard", MessageBoxButtons.OK);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
             textBox1.Clear();
             textBox3.Clear();
             textBox4.Clear();
@@ -48,7 +54,6 @@ namespace ResourceTool
             FileInfo[] Files = dinfo.GetFiles("*.ytd", SearchOption.AllDirectories);
 
             List<string> list = new List<string>();
-
             // Set to false for no duplicates
             textBox4.Text = "FALSE";
 
@@ -81,12 +86,299 @@ namespace ResourceTool
                     AppendLine(textBox1, item);
                 }
             }
+        }        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+
+            if (!string.IsNullOrEmpty(textBox2.Text))
+            {
+                RPath = textBox2.Text;
+            }
+            //If nmsPath is not found, show message and return
+            if (!Directory.Exists(RPath))
+            {
+                MessageBox.Show("Path not Found!", "Alert", MessageBoxButtons.OK);
+                return;
+            }
+
+            //Search for hg files in the current dir
+            DirectoryInfo dinfo = new DirectoryInfo(RPath);
+            //FileInfo[] Files = dinfo.GetFiles("*.ytd", SearchOption.AllDirectories);
+            FileInfo[] Files = dinfo.GetFiles("*.*", SearchOption.AllDirectories);
+
+            List<string> list = new List<string>();
+
+            // Set to false for no duplicates
+            textBox4.Text = "FALSE";
+
+            //if hg files are found, start adding them to dictionaries
+            if (Files.Length != 0)
+            {
+                foreach (FileInfo file in Files.OrderByDescending(f => f.LastWriteTime))
+                {
+                    //if (!file.Name.Contains("+") && !file.Name.Contains("vehicles") && !file.Name.Contains("share"))
+                    //{
+                    string name = Path.GetFileNameWithoutExtension(file.FullName);
+
+                    if (!list.Contains(file.Name))
+                    {
+                        list.Add("\"" + file.Name + "\",");
+                    }
+                    else
+                    {
+                        // Show true if duplicates are found
+                        textBox4.Text = "TRUE";
+                    }
+                    //}                    
+                }
+
+                // Show how many files
+                textBox3.Text = list.Count.ToString();
+
+                foreach (string item in list)
+                {
+                    AppendLine(textBox1, item);
+                }
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
-            Clipboard.SetText(textBox1.Text);
-            MessageBox.Show("Copied to Clipboard!", "Clipboard", MessageBoxButtons.OK);
+            textBox1.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+
+            if (!string.IsNullOrEmpty(textBox2.Text))
+            {
+                RPath = textBox2.Text;
+            }
+            //If nmsPath is not found, show message and return
+            if (!Directory.Exists(RPath))
+            {
+                MessageBox.Show("Path not Found!", "Alert", MessageBoxButtons.OK);
+                return;
+            }
+
+            //Search for hg files in the current dir
+            DirectoryInfo dinfo = new DirectoryInfo(RPath);
+            //FileInfo[] Files = dinfo.GetFiles("*.ytd", SearchOption.AllDirectories);
+            FileInfo[] Files = dinfo.GetFiles("*.*", SearchOption.AllDirectories);
+
+            List<string> list = new List<string>();
+
+            // Set to false for no duplicates
+            textBox4.Text = "FALSE";
+
+            //if hg files are found, start adding them to dictionaries
+            if (Files.Length != 0)
+            {
+                foreach (FileInfo file in Files.OrderByDescending(f => f.LastWriteTime))
+                {
+                    //if (!file.Name.Contains("+") && !file.Name.Contains("vehicles") && !file.Name.Contains("share"))
+                    //{
+                    string name = Path.GetFileNameWithoutExtension(file.FullName);
+
+                    if (!list.Contains(file.Name))
+                    {
+                        list.Add("\"" + file.Name + "\",");
+                    }
+                    else
+                    {
+                        // Show true if duplicates are found
+                        textBox4.Text = "TRUE";
+                    }
+                    //}                    
+                }
+
+                // Show how many files
+                textBox3.Text = list.Count.ToString();
+
+                foreach (string item in list)
+                {
+                    AppendLine(textBox1, item);
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+
+            if (!string.IsNullOrEmpty(textBox2.Text))
+            {
+                RPath = textBox2.Text;
+            }
+            //If nmsPath is not found, show message and return
+            if (!Directory.Exists(RPath))
+            {
+                MessageBox.Show("Path not Found!", "Alert", MessageBoxButtons.OK);
+                return;
+            }
+
+            //Search for hg files in the current dir
+            DirectoryInfo dinfo = new DirectoryInfo(RPath);
+            //FileInfo[] Files = dinfo.GetFiles("*.ytd", SearchOption.AllDirectories);
+            DirectoryInfo[] dirs = dinfo.GetDirectories("*.*", SearchOption.AllDirectories);
+
+            List<string> list = new List<string>();
+
+            // Set to false for no duplicates
+            textBox4.Text = "FALSE";
+
+            //if hg files are found, start adding them to dictionaries
+            if (dirs.Length != 0)
+            {
+                foreach (DirectoryInfo dir in dirs.OrderByDescending(f => f.LastWriteTime))
+                {
+                    if (!dir.Name.Contains("stream") && !dir.Name.Contains("["))
+                    {
+                    string name = Path.GetFileNameWithoutExtension(dir.FullName);
+
+                    if (!list.Contains(dir.Name))
+                    {
+                        list.Add("\"" + dir.Name + "\",");
+                    }
+                    else
+                    {
+                        // Show true if duplicates are found
+                        textBox4.Text = "TRUE";
+                    }
+                    }                    
+                }
+
+                // Show how many files
+                textBox3.Text = list.Count.ToString();
+
+                foreach (string item in list)
+                {
+                    AppendLine(textBox1, item);
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+
+            if (!string.IsNullOrEmpty(textBox2.Text))
+            {
+                RPath = textBox2.Text;
+            }
+            //If nmsPath is not found, show message and return
+            if (!Directory.Exists(RPath))
+            {
+                MessageBox.Show("Path not Found!", "Alert", MessageBoxButtons.OK);
+                return;
+            }
+
+            //Search for hg files in the current dir
+            DirectoryInfo dinfo = new DirectoryInfo(RPath);
+            //FileInfo[] Files = dinfo.GetFiles("*.ytd", SearchOption.AllDirectories);
+            FileInfo[] Files = dinfo.GetFiles("*.ymap", SearchOption.AllDirectories);
+
+            List<string> list = new List<string>();
+
+            // Set to false for no duplicates
+            textBox4.Text = "FALSE";
+
+            //if hg files are found, start adding them to dictionaries
+            if (Files.Length != 0)
+            {
+                foreach (FileInfo file in Files.OrderByDescending(f => f.LastWriteTime))
+                {
+                    //if (!file.Name.Contains("+") && !file.Name.Contains("vehicles") && !file.Name.Contains("share"))
+                    //{
+                    string name = Path.GetFileNameWithoutExtension(file.FullName);
+
+                    if (!list.Contains(file.Name))
+                    {
+                        list.Add("\"" + file.Name + "\",");
+                    }
+                    else
+                    {
+                        // Show true if duplicates are found
+                        textBox4.Text = "TRUE";
+                    }
+                    //}                    
+                }
+
+                // Show how many files
+                textBox3.Text = list.Count.ToString();
+
+                foreach (string item in list)
+                {
+                    AppendLine(textBox1, item);
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+
+            if (!string.IsNullOrEmpty(textBox2.Text))
+            {
+                RPath = textBox2.Text;
+            }
+            //If nmsPath is not found, show message and return
+            if (!Directory.Exists(RPath))
+            {
+                MessageBox.Show("Path not Found!", "Alert", MessageBoxButtons.OK);
+                return;
+            }
+
+            //Search for hg files in the current dir
+            DirectoryInfo dinfo = new DirectoryInfo(RPath);
+            FileInfo[] Files = dinfo.GetFiles("*.ytd", SearchOption.AllDirectories);
+
+            List<string> list = new List<string>();
+            // Set to false for no duplicates
+            textBox4.Text = "FALSE";
+
+            //if hg files are found, start adding them to dictionaries
+            if (Files.Length != 0)
+            {
+                foreach (FileInfo file in Files.OrderByDescending(f => f.LastWriteTime))
+                {
+                    if (!file.Name.Contains("+") && !file.Name.Contains("vehicles") && !file.Name.Contains("share"))
+                    {
+                        string name = Path.GetFileNameWithoutExtension(file.FullName);
+
+                        if (!list.Contains(name))
+                        {
+                            list.Add(@"<Item><Name>" + name + @"</Name><Variations type=""NULL""/></Item>");
+                        }
+                        else
+                        {
+                            // Show true if duplicates are found
+                            textBox4.Text = "TRUE";
+                        }
+                    }
+                }
+
+                // Show how many files
+                textBox3.Text = list.Count.ToString();
+
+                foreach (string item in list)
+                {
+                    AppendLine(textBox1, item);
+                }
+            }
         }
     }
 }
